f6e77fcc2df4a82b95ff323d765490b15b9fd5c2
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-17 15:18:16 +0100

    Basics working

---
+++ example/fs.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
+console.log(
+  fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
+)
--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log('Hello from Bare')
+/* global Bare */
+const Module = require('bare-module')
+const path = require('bare-path')
+const os = require('bare-os')
+const process = require('bare-process')
+const pkg = require('../package.json')
+
+global.process = process
+
+if (Bare.argv.length === 1) {
+  require('bare-repl').start()
+} else {
+  const imports = pkg.imports
+
+  Module.load(
+    Bare.argv[1] = Module.resolve(
+      path.resolve(os.cwd(), Bare.argv[1]),
+      { imports }
+    ),
+    { imports }
+  )
+}
