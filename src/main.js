729e840a1e94054b3e9ce7dc19cd5f188e65a9f5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-20 11:06:26 +0100

    Use new public `builtins` API

--- src/main.js
+++ src/main.js
@@ -3,20 +3,33 @@ const Module = require('bare-module')
 const path = require('bare-path')
 const os = require('bare-os')
 const process = require('bare-process')
-const pkg = require('../package.json')
+const repl = require('bare-repl')
 
 global.process = process
 
-if (Bare.argv.length === 1) {
-  require('bare-repl').start()
-} else {
-  const imports = pkg.imports
+const builtins = {
+  module: Module,
+  path,
+  os,
+  process,
+  repl,
 
+  assert: require('bare-assert'),
+  console: require('bare-console'),
+  events: require('bare-events'),
+  fs: require('bare-fs'),
+  tty: require('bare-tty'),
+  url: require('bare-url')
+}
+
+if (Bare.argv.length === 1) {
+  repl.start()
+} else {
   Module.load(
     Bare.argv[1] = Module.resolve(
       path.resolve(os.cwd(), Bare.argv[1]),
-      { imports }
+      { builtins }
     ),
-    { imports }
+    { builtins }
   )
 }
